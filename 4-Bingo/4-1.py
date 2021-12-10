a3cc9058403ca0f7dd80fc4cceba03d1e2e72156
Jared Wood <Jared.wood13@gmail.com>
2021-12-10 14:28:25 +1100

    cleanup

--- 4-Bingo/4-1.py
+++ 4-Bingo/4-1.py
@@ -4,18 +4,15 @@ from Bingo_Board import Board
 boards = []
 with open("input.txt") as file:
     random_sequence = [int(x) for x in file.readline().strip().split(',')]
-    # try:
-    while file.readline(): # Blank
+    while file.readline():
         board = np.zeros((5,5),dtype=int)
         for i in range(5):
             
             line = file.readline().strip().replace('  ',' ').split(' ',)
-            print(line)
             board[i,:] = line 
 
         boards.append(Board(board))
-    # except:
-print(boards)
+
 
 for bingo_call in random_sequence:
     print(bingo_call)
@@ -23,7 +20,6 @@ for bingo_call in random_sequence:
         i, j = board.mark_number(bingo_call)
         if i >= 0:
             if board.check_win(i,j):
-                print(np.sum(board.board))
                 print(bingo_call,board,board.sum_unmarked(),bingo_call*board.sum_unmarked())
                 break
     else:
