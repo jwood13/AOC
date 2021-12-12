330d28d7aeedfd7e9a63b2add317a7137663758d
jared Wood <jared.wood@sydney.edu.au>
2021-12-13 10:32:05 +1100

    Day 6 part 1

---
+++ 6-Lanternfish/6-1.py
@@ -0,0 +1,15 @@
+import numpy as np
+age_list = np.zeros(9)
+with open("input.txt") as file:
+    line = file.readline()
+    for age in line.strip().split(','):
+        age_list[int(age)] +=1
+
+for day in range(80):
+    births = age_list[0]
+    for i in range(8):
+        age_list[i] = age_list[i+1]
+    age_list[8] = births
+    age_list[6] += births
+print(np.sum(age_list))
+print(age_list)
