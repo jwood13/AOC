fc7c73dc5a91fabe4bd17cde671e1840f682ef09
jared Wood <jared.wood@sydney.edu.au>
2021-12-16 12:29:25 +1100

    Day 11 Part 2

--- 11-Dumbo-Octopus/11-2.py
+++ 11-Dumbo-Octopus/11-2.py
@@ -18,7 +18,10 @@ def flash(cave, i, j):
 
 
 flashes = 0
-for step in range(100):
+simultaneous_flash = False
+step = 0
+while not simultaneous_flash:
+    step += 1
     cave += 1
     flashed = np.zeros_like(cave, dtype=bool)
     check_again = True
@@ -31,9 +34,13 @@ for step in range(100):
                     flashed[i, j] = True
                     check_again = True
                     flashes += 1
+    print(np.sum(flashed))
+    if np.sum(flashed) == (width-2)*(height-2):
+        simultaneous_flash = True
     for i in range(height):
         for j in range(width):
             if cave[i, j] > 9:
                 cave[i, j] = 0
 print(cave[1:height-1, 1:width-1])
 print(flashes)
+print(step)
