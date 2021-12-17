1c665665494cdb2f28fa85c0bd9c6da68071e390
jared Wood <jared.wood@sydney.edu.au>
2021-12-17 16:19:43 +1100

    Day 12 Part 2

---
+++ 12-Passage_Pathing/12-2.py
@@ -0,0 +1,23 @@
+from collections import defaultdict
+accessible = defaultdict(list)
+for line in open("input.txt").readlines():
+    a, b = line.strip().split('-')
+    accessible[a].append(b)
+    accessible[b].append(a)
+print(accessible)
+paths = ['start']
+complete_paths = []
+while len(paths) != 0:
+    new_paths = []
+    for path in paths:
+        tail = path.split('-')[-1]
+        for addition in accessible[tail]:
+            if addition == 'end':
+                complete_paths.append(path+'-'+addition)
+            elif addition not in path or addition.upper() == addition:
+                new_paths.append(path+'-'+addition)
+            elif len(addition) == 2 and path[0] != 'X':
+                new_paths.append('X'+path+'-'+addition)
+    paths = new_paths
+print(complete_paths)
+print(len(complete_paths))
